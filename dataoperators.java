27f02aad162fee1b19a905ed49e95bf380c829f9
vasumathikv2007-lang <vasumathikv2007@gmail.com>
2026-01-22 15:14:18 +0530

    Add files via upload

---
+++ dataoperators.java
@@ -0,0 +1,30 @@
+public class dataoperators{
+public static void main (String[]args){
+int a=100;
+int b=10;
+boolean x=true;
+boolean y=false;
+System.out.println("Arithmetic Operators:");
+System.out.println("a+b ="+ (a+b));
+System.out.println("a-b ="+ (a-b));
+System.out.println("a/b ="+ (a/b));
+System.out.println("a*b ="+ (a*b));
+System.out.println("a%b ="+ (a%b));
+
+System.out.println("a>b ="+ (a>b));
+System.out.println("a<b = " + (a<b));
+System.out.println("a==b = " + (a == b));
+System.out.println("a!=b = " + (a != b));
+
+System.out.println("\nLogical Operators:");
+System.out.println("x && y = " + (x && y));
+System.out.println("x || y = " + (x || y));
+System.out.println("!x = " + (!x));
+
+
+int d=45;
+System.out.println("d = " + d);
+System.out.println("++d = " + (++d));
+System.out.println("--d = " + (--d));
+}}
+
---
+++ datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes{
+public static void main(String[]args)
+{
+int i=777;
+boolean o=true;
+double d=364.576;
+float f=3.14f;
+char c='A';
+byte b = 100;            
+short s = 10000;                 
+long l = 1000000000L;
+System.out.println("integer value:"+i);
+System.out.println("Boolean value:"+o); 
+System.out.println("float value:"+f); 
+System.out.println("double value:"+d); 
+System.out.println("byte value:"+b);  
+System.out.println("short value:"+s);  
+System.out.println("long value:"+l);   
+System.out.println("char value: " + c); 
+                                 }}
